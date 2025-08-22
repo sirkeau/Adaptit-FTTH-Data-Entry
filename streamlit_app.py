1dd8af99216491954a6d572f5904c22ec4b2b1ad
Christoforos Sirkoglou <csir@adaptit.com>
2025-08-22 15:45:58 +0300

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,14 @@ import streamlit as st
 
 st.title("🎈 My new app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/). test test"
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+st.write("""
+This is a test.
+""")
+
+st.markdown("""
+- one test
+- two test
+""")
